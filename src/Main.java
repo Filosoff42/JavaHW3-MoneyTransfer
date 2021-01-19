fb9f582f5fbdb3bf3df5f43b52347b498d69a4ea
Grigoriy Kharitonskiy <argh94@list.ru>
2021-01-19 08:37:00 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int sum = 500_000_000;
+        int transfer = balance + sum;
+        System.out.println("Текущий баланс = " + transfer);
+    }
+}
